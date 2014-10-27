3d45a6eaebad4f41da6bc45650451fc8e602f846
wakko89 <wakko4twin@hotmail.com>
2014-10-27 03:42:34 +0100

    Update
    Changed trigger and untrigger conditions

--- trunk/WA Adehle eventMB v1.0.lua
+++ trunk/WA Adehle eventMB v1.0.lua
@@ -18,15 +18,17 @@ function()
     local _,dGCD = GetSpellCooldown(spellGCD)
     local sTime,CD = GetSpellCooldown(spell)
     
-    if CD ~= dGCD or CD == 0 then
-		if CD == dGCD then -- if spell ready
-			WA_ADEHLE_EVENTMB_END = WA_ADEHLE_EVENTMB_TIME
-		else -- if spell on cooldown
-			WA_ADEHLE_EVENTMB_START = sTime
-			WA_ADEHLE_EVENTMB_DURA = CD
-			WA_ADEHLE_EVENTMB_END = WA_ADEHLE_EVENTMB_START + WA_ADEHLE_EVENTMB_DURA
-			WA_ADEHLE_EVENTMB_TIME = GetTime()
-		end
+	if CD == dGCD then -- if spell ready
+		WA_ADEHLE_EVENTMB_END = WA_ADEHLE_EVENTMB_TIME or 0
+	else -- if spell on cooldown
+		WA_ADEHLE_EVENTMB_START = sTime
+		WA_ADEHLE_EVENTMB_DURA = CD
+		WA_ADEHLE_EVENTMB_END = WA_ADEHLE_EVENTMB_START + WA_ADEHLE_EVENTMB_DURA
+		WA_ADEHLE_EVENTMB_TIME = GetTime()
+	end
+	local endTime = WA_ADEHLE_EVENTMB_END or 0
+	local past = WA_ADEHLE_EVENTMB_PAST or 0
+    if (endTime+past > GetTime()) then
         return true
     end
 end
@@ -37,8 +39,9 @@ end
 function()
 	local startTime = WA_ADEHLE_EVENTMB_START or 0
 	local dura = WA_ADEHLE_EVENTMB_DURA or 0
+	local endTime = WA_ADEHLE_EVENTMB_END or 0
 	local past = WA_ADEHLE_EVENTMB_PAST or 0
-    if not (GetTime() > startTime + dura + past) then
+    if not (endTime + past > GetTime()) then
         return true
     end
 end
